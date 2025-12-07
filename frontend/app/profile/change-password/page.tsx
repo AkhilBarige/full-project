018a86f11bcc78643e12410da832c1dd3940b708
AkhilBarige <akhilbarige21@gmail.com>
2025-12-07 15:56:02 +0530

    password checks

--- frontend/app/profile/change-password/page.tsx
+++ frontend/app/profile/change-password/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import ChangePasswordForm from "../../components/ChangePasswordForm";
+import ChangePasswordForm from "../../../components/ChangePasswordForm";
 import { useRouter } from "next/navigation";
 
 export default function ChangePasswordPage() {
